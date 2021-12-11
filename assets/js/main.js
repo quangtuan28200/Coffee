91a0dbf7bbc10e3755b7ff2aae9c8fbc6ab01196
unknown <buiquangtuan02082000@gmail.com>
2021-12-11 10:35:34 +0700

    HTML-CSS-done

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,40 @@
 var header = document.querySelector("#header");
 
 window.onscroll = () => {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         header.classList.add("scrolled");
-    }else{
+    } else {
         header.classList.remove("scrolled");
 
     }
 }
+
+//counter
+
+const counterAnim = (qSelector, start = 0, end, duration = 1000) => {
+    const target = document.querySelector(qSelector);
+    let startTimestamp = null;
+    const step = (timestamp) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        target.innerText = Math.floor(progress * (end - start) + start);
+        if (progress < 1) {
+            window.requestAnimationFrame(step);
+        }
+    };
+    window.requestAnimationFrame(step);
+};
+
+var review = document.querySelector("#review");
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    counterAnim("#counter1", 0, 2536, 1500);
+    counterAnim("#counter2", 0, 7562, 1500);
+    counterAnim("#counter3", 0, 2013, 1500);
+    counterAnim("#counter4", 0, 10536, 1500);
+});
+
 
 
 
