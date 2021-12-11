c94738500a8f60fdf37d59b98f21da9919c06d7f
unknown <buiquangtuan02082000@gmail.com>
2021-12-11 14:56:14 +0700

    JS-done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,22 @@
 //check header scrolled
 var header = document.querySelector("#header");
+var review = document.querySelector("#review");
+var checkCounter = false;
+const reviewOffsetTop = review.offsetTop;
 
 window.onscroll = () => {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    const windowOffsetTop = document.documentElement.scrollTop;
+    if (windowOffsetTop > 50) {
         header.classList.add("scrolled");
-    } else {
+        if(windowOffsetTop >= reviewOffsetTop && !checkCounter){
+            checkCounter = true;
+            counterAnim("#counter1", 0, 2536, 800);
+            counterAnim("#counter2", 0, 7562, 800);
+            counterAnim("#counter3", 0, 2013, 800);
+            counterAnim("#counter4", 0, 10536, 800);
+        }
+    }else {
         header.classList.remove("scrolled");
-
     }
 }
 
@@ -16,25 +26,21 @@ const counterAnim = (qSelector, start = 0, end, duration = 1000) => {
     const target = document.querySelector(qSelector);
     let startTimestamp = null;
     const step = (timestamp) => {
-        if (!startTimestamp) startTimestamp = timestamp;
+        if (!startTimestamp) {
+            startTimestamp = timestamp;
+        }
+        // console.log('timestamp: ',timestamp)
+        // console.log('startTimestamp: ',startTimestamp)
         const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        // console.log('progress: ',progress)
         target.innerText = Math.floor(progress * (end - start) + start);
+        // console.log('value: ',Math.floor(progress * (end - start) + start))
         if (progress < 1) {
             window.requestAnimationFrame(step);
         }
     };
     window.requestAnimationFrame(step);
 };
-
-var review = document.querySelector("#review");
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    counterAnim("#counter1", 0, 2536, 1500);
-    counterAnim("#counter2", 0, 7562, 1500);
-    counterAnim("#counter3", 0, 2013, 1500);
-    counterAnim("#counter4", 0, 10536, 1500);
-});
 
 
 
